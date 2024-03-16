067eab3ddddfcb9560ef00f2f9c09931980761fc
tanvirmahamudshakil <shakilhassan887@gmail.com>
2024-03-16 23:59:35 +0600

    first commit

---
+++ football_1/football_1App.swift
@@ -0,0 +1,17 @@
+//
+//  football_1App.swift
+//  football_1
+//
+//  Created by Tanvir on 21/2/24.
+//
+
+import SwiftUI
+
+@main
+struct football_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
